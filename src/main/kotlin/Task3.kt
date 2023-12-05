9ad2881b35ea18fcac0355797a611de52769e40b
vladimir <VEMatveev@alfabank.ru>
2023-12-05 23:03:57 +0500

    Lesson 8 task 3 fix

--- src/main/kotlin/Task3.kt
+++ src/main/kotlin/Task3.kt
@@ -5,11 +5,11 @@ fun main() {
     println("Введите ингедиент")
     val inputUser = readln()
 
-    when(inputUser) {
-        ingredient[0] -> println("Ингрединет ${ingredient[0]} есть в Текила санрайз")
-        ingredient[1] -> println("Ингрединет ${ingredient[1]} есть в Текила санрайз")
-        ingredient[2] -> println("Ингрединет ${ingredient[2]} есть в Текила санрайз")
-        ingredient[3] -> println("Ингрединет ${ingredient[3]} есть в Текила санрайз")
-        else -> println("Ингредиент не найдет")
+    for (i in ingredient) {
+        if (i == inputUser) {
+            println("Ингрединет ${inputUser} есть в Текила санрайз")
+            return
+        }
     }
+    println("Ингредиент не найдет")
 }
